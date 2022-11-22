3c8712d47edd73171176be2f99522f68db553819
Jongkuk Lim <lim.jeikei@gmail.com>
2022-11-22 19:07:40 +0900

    refactored cross_compile.sh for better readability (#13)
    Co-authored-by: JeiKeiLim <limjk@jmarple.ai>

--- cross_compile.sh
+++ cross_compile.sh
@@ -6,10 +6,10 @@ ARCHS=("amd64" "arm64" "arm")
 
 for os in ${OSS[@]}; do
     for arch in ${ARCHS[@]}; do
-        echo "Compile $os-$arch ..."
-        env GOOS=$os GOARCH=$arch go build ./cmd/ducker
-        if [ $? -eq 0 ]; then
+        echo -n "Compile $os-$arch ... "
+        if env GOOS=$os GOARCH=$arch go build ./cmd/ducker; then
             tar -czf ducker-$VERSION-$os-$arch.tar.gz ./ducker
+            echo "ducker-$VERSION-$os-$arch.tar.gz has been created."
         fi
     done
 done
