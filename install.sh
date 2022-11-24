067f255668e4ec1ea0c0eb46522722f467ff9ebf
JeiKeiLim <limjk@jmarple.ai>
2022-11-24 10:26:05 +0900

    Add command arg test script.

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ VERSION=0.1.3
 OS=linux
 ARCH=amd64
 
+echo $@
+exit 0;
+
 wget https://github.com/JeiKeiLim/ducker/releases/download/v$VERSION/ducker-$VERSION-$OS-$ARCH.tar.gz
 tar xzvf ducker-$VERSION-$OS-$ARCH.tar.gz
 sudo mv ducker /usr/local/bin/
