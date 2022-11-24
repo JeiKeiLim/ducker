bd9e820f9189fc09ace41db88aaded187156ad4c
JeiKeiLim <limjk@jmarple.ai>
2022-11-24 10:19:27 +0900

    (WIP) Add install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+VERSION=0.1.3
+OS=linux
+ARCH=amd64
+
+wget https://github.com/JeiKeiLim/ducker/releases/download/v$VERSION/ducker-$VERSION-$OS-$ARCH.tar.gz
+tar xzvf ducker-$VERSION-$OS-$ARCH.tar.gz
+sudo mv ducker /usr/local/bin/
+rm ducker-$VERSION-$OS-$ARCH.tar.gz
+
